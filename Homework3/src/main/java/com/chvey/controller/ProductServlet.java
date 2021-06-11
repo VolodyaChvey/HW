ac2fdde5623d733cbfb7ee3ebcd0d696ec808180
VolodyaChvey <volodya76@tut.by>
2021-06-11 19:45:09 +0300

    db1

--- Homework3/src/main/java/com/chvey/controller/ProductServlet.java
+++ Homework3/src/main/java/com/chvey/controller/ProductServlet.java
@@ -6,14 +6,13 @@ import com.chvey.service.UserService;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.io.PrintWriter;
+
 import java.util.Map;
 
 @WebServlet(name = "ProductServlet", urlPatterns = {"/servlet2"})
--- Homework3/src/main/java/com/chvey/sql/SqlHelper.java
+++ Homework3/src/main/java/com/chvey/sql/SqlHelper.java
@@ -18,9 +18,10 @@ public class SqlHelper {
     }
 
     public static void main(String[] args) {
+        initDB();
         Connection conn = SqlHelper.getConnection();
-        try {
-            PreparedStatement ps = conn.prepareStatement("SELECT * FROM TEST");
+       try {
+            PreparedStatement ps = conn.prepareStatement("SELECT * FROM USER");
             ResultSet resultSet = ps.executeQuery();
             while (resultSet.next()) {
                 int id = resultSet.getInt("id");
@@ -30,10 +31,38 @@ public class SqlHelper {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+       /* try (PreparedStatement ps = conn.prepareStatement("SELECT * FROM USER")) {
+            ResultSet resultSet = ps.executeQuery();
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                System.out.printf("%d. %s", id, name + "\n");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }*/
 
     }
 
     public static void initDB() {
         Connection conn = SqlHelper.getConnection();
+        try {
+            Statement st= conn.createStatement();
+            st.executeUpdate(
+                    "DROP TABLE IF EXISTS USER;" +
+                            "CREATE TABLE USER" +
+                            "(ID INT PRIMARY KEY AUTO_INCREMENT," +
+                            "NAME VARCHAR(255) NOT NULL," +
+                            "PASSWORD VARCHAR(255));" +
+                            "INSERT INTO USER(NAME)" +
+                            "VALUES('Vasya');"
+
+            );
+
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
