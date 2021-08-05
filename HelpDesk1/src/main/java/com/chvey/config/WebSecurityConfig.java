c340447b40153e5b6871b59162ee4c538a7efe11
VolodyaChvey <volodya76@tut.by>
2021-08-05 17:44:36 +0300

    helpdesk3 commit

--- HelpDesk1/src/main/java/com/chvey/config/WebSecurityConfig.java
+++ HelpDesk1/src/main/java/com/chvey/config/WebSecurityConfig.java
@@ -13,8 +13,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Collections;
 
 
 @Configuration
@@ -55,11 +57,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-  /*  CorsConfigurationSource corsConfigurationSource(){
-        CorsConfigurationSource source = new CorsConfigurationSource() {
-
-        }
-    }*/
-
+    
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE","PUT"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
